e395cb3d5e8aa908424980d5224f41dee1eb7856
Michael <mejevin@gmail.com>
2020-01-25 22:44:36 +0300

    fuck

---
+++ Source/ResxSync/ResxSync.UI/Lib/ResxCollection.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Resources;
+
+namespace ResxSync.UI.Lib
+{
+    public class ResxCollection
+    {
+        Resx
+
+        public ResxCollection()
+        {
+
+        }
+
+        public void Add(string filePath)
+        {
+
+        }
+    }
+}
