c22d9b924aea38f1d3d66999ce227468e0d50998
Michael <mejevin@gmail.com>
2020-01-25 20:16:14 +0300

    add: MVVM support with caliburn.micro

---
+++ Source/ResxSync/ResxSync.UI/ViewModels/ShellViewModel.cs
@@ -0,0 +1,17 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ResxSync.UI.ViewModels
+{
+    public class ShellViewModel : Conductor<object>
+    {
+
+        public ShellViewModel()
+        {
+            //ActivateItem(IoC.Get<LoginViewModel>());
+        }
+
+    }
+}
