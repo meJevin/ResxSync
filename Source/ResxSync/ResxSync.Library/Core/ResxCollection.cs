07119f4039a58e2ffed7a831f0c499a29e5c2ca0
Michael <mejevin@gmail.com>
2020-01-26 03:36:25 +0300

    woeking

---
+++ Source/ResxSync/ResxSync.Library/Core/ResxCollection.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Resources;
+using System.Collections;
+using System.IO;
+
+namespace ResxSync.Library.Core
+{
+    public class ResxFile
+    {
+        public static ResxFile Load(string path)
+        {
+            return null;
+        }
+
+        public string Name
+        {
+            get;
+        }
+
+        public Dictionary<string, string> KVPs
+        {
+            get;
+        }
+    }
+
+    public class ResxKeyCollection
+    {
+
+    }
+
+    public class ResxCollection
+    {
+        // Loaded .resx files and their KVPs
+        public Dictionary<string, Dictionary<string, string>> _resxFiles;
+
+        // All encountered keys and their values according to file
+        public Dictionary<string, Dictionary<string, string>> _keysAndTheirValues;
+
+        public ResxCollection()
+        {
+            _resxFiles = new Dictionary<string, Dictionary<string, string>>();
+
+            _keysAndTheirValues = new Dictionary<string, Dictionary<string, string>>();
+        }
+
+        public void Add(string filePath)
+        {
+            string resxName = Path.GetFileNameWithoutExtension(filePath);
+
+            if (_resxFiles.ContainsKey(resxName))
+            {
+                return;
+            }
+
+            Dictionary<string, string> keyValuePairs = new Dictionary<string, string>();
+            using (ResXResourceReader resx = new ResXResourceReader(filePath))
+            {
+                foreach (DictionaryEntry entry in resx)
+                {
+                    var key = entry.Key.ToString();
+                    var value = entry.Value.ToString();
+
+                    keyValuePairs.Add(key, value);
+
+                    if (_keysAndTheirValues.ContainsKey(key))
+                    {
+                        // Already encountered, add file, values KVP
+                        _keysAndTheirValues[key].Add(resxName, value);
+                    }
+                    else
+                    {
+                        // New key. Add and populate
+                        Dictionary<string, string> values = new Dictionary<string, string>();
+
+                        foreach (var resxFile in _resxFiles.Keys)
+                        {
+                            values.Add(resxFile, null);
+                        }
+
+                        values.Add(resxName, value);
+
+                        _keysAndTheirValues.Add(key, values);
+                    }
+                }
+
+                // Add nulls to keys that weren't in this file
+                foreach (var key in _keysAndTheirValues.Keys.Except(keyValuePairs.Keys))
+                {
+                    _keysAndTheirValues[key].Add(resxName, null);
+                }
+            }
+
+            _resxFiles.Add(resxName, keyValuePairs);
+        }
+
+        public Dictionary<string, Dictionary<string, string>> GetUnsynced()
+        {
+            Dictionary<string, Dictionary<string, string>> result = new Dictionary<string, Dictionary<string, string>>();
+
+            foreach (var keyAndItsValues in _keysAndTheirValues)
+            {
+                if (keyAndItsValues.Value.Values.Contains(null))
+                {
+                    result.Add(keyAndItsValues.Key, keyAndItsValues.Value);
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Source/ResxSync/ResxSync.UI/MainWindow.xaml.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.Win32;
+using ResxSync.Library.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ResxSync.UI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ResxCollection col = new ResxCollection();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void AddFileButtonClick(object sender, RoutedEventArgs e)
+        {
+            #region Get file
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "Resx (*.resx)|*.resx";
+            ofd.RestoreDirectory = true;
+
+            var res = ofd.ShowDialog();
+            if (!res.HasValue || res.Value != true) return;
+            #endregion
+
+            col.Add(ofd.FileName);
+
+            AllKeys.Items.Clear();
+
+            AllKeys.Items.Add(String.Join("\t", col._resxFiles.Keys));
+
+            foreach (var key in col._keysAndTheirValues)
+            {
+                string lbResult = key.Key;
+
+                foreach (var value in key.Value)
+                {
+                    lbResult += "\t" + value.Value;
+                }
+                AllKeys.Items.Add(lbResult);
+            }
+        }
+
+        private void GetUnsyncedButtonClick(object sender, RoutedEventArgs e)
+        {
+            var unsynced = col.GetUnsynced();
+
+            UnsyncedKeys.Items.Clear();
+
+            UnsyncedKeys.Items.Add(String.Join("\t", col._resxFiles.Keys));
+            foreach (var key in unsynced)
+            {
+                string lbResult = key.Key;
+
+                foreach (var value in key.Value)
+                {
+                    lbResult += "\t" + value.Value;
+                }
+                UnsyncedKeys.Items.Add(lbResult);
+            }
+        }
+    }
+}
