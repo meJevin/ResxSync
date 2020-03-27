c57bcb5561ab4dfd1e19dbdc3f8486bbd63ea3e9
Michael <mejevin@gmail.com>
2020-03-28 00:03:45 +0300

    setup empty caliburn micro

---
+++ Source/ResxSync/ResxSync.UI/ViewModels/MainViewModel.cs
@@ -0,0 +1,17 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResxSync.UI.ViewModels
+{
+    public class MainViewModel : Conductor<Screen>
+    {
+        public MainViewModel()
+        {
+
+        }
+    }
+}
